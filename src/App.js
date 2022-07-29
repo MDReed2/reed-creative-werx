6686f3af9beb4494a9bce8132bae803455d4f3b1
mdreed2 <meshia.d.reed@gmail.com>
2022-07-29 12:12:21 -0700

    Header component code and style updates

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.scss';
+import NavbarComponent from './Components/Navbar/NavbarComponent'
+// import NavbarComponent from '../Navbar/NavbarComponent'
+import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
 import Header from './Components/Header/Header'
-import Footer from './Components/Footer/Footer';
+import Footer from './Components/Footer/Footer'
 
 
 function App() {
   return (
     <div>
       {/* <h1>Reed Creative Werx</h1> */}
-      <Header />
+      <NavbarComponent />
+      <Routes>
+        <Route path="/" element={<Header/>} />
+        <Route path="/software-development" />
+        <Route path="/photography" />
+      </Routes>
+      
       <Footer />
     </div>
   )
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,29 +1,38 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './header.scss'
-import NavbarComponent from '../Navbar/NavbarComponent'
-import Hero from '../Hero/Hero'
-// import IMG from '../../Assets/Images/header-img.png'
+
+// import Hero from '../Hero/Hero'
+// import SWE from '../../Assets/Images/SWE-Header.jpeg'
 
 const Header = () => {
   return (
     <div>
-      <NavbarComponent />
+      {/* <NavbarComponent /> */}
 
       <div className='header-container'>
         <div className='header-img' alt='desktop imgae'>
-          <div><Hero/></div>
+          {/* <div><Hero/></div> */}
         </div>
-        <div className='grid-2-col'>
-          <a href='/software-development' className='left'>
-            <h2>Software Development</h2>
-          </a>
-          <a href='/photography' className='right'>
-            <h2>Photography</h2>
-          </a>
-        </div>
-        <div className='header-intro'>
+        <div className='intro-area-text'>
           <p>Hello, I'm Meshia Reed.</p>
           <p>I develop, and shoot photography.</p>
+        </div>
+        <div className='grid-2-col'>
+          <Link to="/software-development" className='left'>
+            <div>
+              <h2>Software Development</h2>
+            </div>
+          </Link>
+          <Link to="/photography" className='right'>
+            <div>
+              <h2>Photography</h2>
+            </div>
+          </Link>
+        </div>
+        <div className='header-intro'>
+          {/* <p>Hello, I'm Meshia Reed.</p>
+          <p>I develop, and shoot photography.</p> */}
           <p>I am a creative mind. I have five years of experiene as a Software Engineer utilizing MERN stack technologies. I am passionate about enhancing the user experince one line at a time. When I am not coding I am looking to create beautiful works of art through a lens. Night photography, product, interior design speak volumes for me.</p>
         </div>
       </div>
