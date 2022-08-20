3f2cede75edc966e579e03e3c8e4854787a7cfb3
mdreed2 <meshia.d.reed@gmail.com>
2022-08-20 13:08:36 -0500

    continued software dev  code structure

--- src/Components/SoftwareDeveloper/SoftwareDeveloper.jsx
+++ src/Components/SoftwareDeveloper/SoftwareDeveloper.jsx
@@ -1,6 +1,38 @@
 import React from 'react'
 import './softwareDev.scss'
 import CV from '../../Assets/MeshiaReed_Resume.pdf'
+import TRUEFOODIE from '../../Assets/Images/true-foodie.png'
+import JOTIT from '../../Assets/Images/jotit.png'
+import TICTACTOE from '../../Assets/Images/tictactoe.png'
+
+import Card from 'react-bootstrap/Card'
+
+const data = [
+  {
+    id: 1,
+    image: TRUEFOODIE,
+    title: 'True Foodie',
+    description: 'HTML ◦ CSS ◦ JavaScript ◦ Express',
+    github: 'https://github.com/MDReed2/true-foodie-client',
+    demo: 'https://mdreed2.github.io/true-foodie-client/'
+  },
+  {
+    id: 2,
+    image: JOTIT,
+    title: 'Jotit',
+    description: 'HTML ◦ CSS ◦ JavaScript ◦ Express ◦ React',
+    github: 'https://github.com/MDReed2/project-frontend',
+    demo: 'https://house-slytherin.github.io/project-frontend/'
+  },
+  {
+    id: 3,
+    image: TICTACTOE,
+    title: 'Tic Tac Toe',
+    description: 'HTML ◦ CSS ◦ JavaScript',
+    github: 'https://github.com/MDReed2/TicTacToe-Game',
+    demo: 'https://mdreed2.github.io/TicTacToe-Game/'
+  }
+]
 
 const SoftwareDeveloper = () => {
   return (
@@ -14,9 +46,25 @@ const SoftwareDeveloper = () => {
         </div>
       </section>
       <section className='main'>
-        <div className='about'></div>
+        <div className='about'>
+          
+        </div>
         <div className='experience'></div>
-        <div className='projects'></div>
+        <div className='projects'>
+          <div className='card-section'>
+            {data.map(({id, image, title, description, github, demo}) => {
+              return (
+                <Card key={id}>
+                  <Card.Img src={image} />
+                  <Card.Body>
+                    <Card.Title>{title}</Card.Title>
+                    <Card.Text>{description}</Card.Text>
+                  </Card.Body>
+                </Card>
+              )
+            })}
+          </div>
+        </div>
         <div className='contact'></div>
       </section>
     </div>
